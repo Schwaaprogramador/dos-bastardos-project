ac45d6518edf35f56c0d06a365e0bf1b63ecd118
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-02-20 00:13:50 -0500

    configuracion inicial

---
+++ dos-bastardos-project/src/App.js
@@ -0,0 +1,12 @@
+import LandingPage from "./componentes/LandingPage/LandingPage";
+
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ dos-bastardos-project/src/componentes/LandingPage/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './LandingPage.module.css'
+
+function LandingPage() {
+  return (
+    <div className={styles.container}>
+        <div>
+            <button>Entrar</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default LandingPage
