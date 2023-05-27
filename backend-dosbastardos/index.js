73b07f974065095e482d6c92a72c9aedd7261a28
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-05-27 18:36:44 -0500

    Mongo connect

--- backend-dosbastardos/index.js
+++ backend-dosbastardos/index.js
@@ -1,21 +1,31 @@
 const express = require('express')
 const app = express();
 const morgan = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
+const mongoose = require('mongoose');
+const userModel = require('./Models/User.js')
 
+//----------MIDDLEWARES---------------
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json())
+
+//-----------Mongose--------
+mongoose.connect('mongodb+srv://danieltoroprogramacion:Qq7812*@cluster0.9lyfv2a.mongodb.net/?retryWrites=true&w=majority')
 
 app.get('/test', (req, res)=>{
     res.send('test ok')
 })
 
-app.post('/registro', (req, res)=>{
+
+app.post('/registro', async (req, res)=>{
     const {username, password} = req.body;
+    const userDoc = await userModel.create({ username, password})
     console.log(req.body);
-    res.send('registro ok')
+    res.send(userDoc)
 })
+
+
 
 
 app.listen(3001, () => {
