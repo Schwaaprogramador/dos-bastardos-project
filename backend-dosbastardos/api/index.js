347c1b746a80624fe12a421ac11b164a42109f38
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-06-11 16:54:58 -0500

    cors fixes1.1

--- backend-dosbastardos/api/index.js
+++ backend-dosbastardos/api/index.js
@@ -28,7 +28,7 @@
     app.use(morgan('dev'));
 
     app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "https://dos-bastardos-project.vercel.app"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
         res.header("Access-Control-Allow-Credentials", "true");
         res.header(
             "Access-Control-Allow-Headers",
@@ -146,7 +146,7 @@ app.get('/profile', (req, res)=>{
 
 //------------------------NUEVO POST-----------------------
 
-app.post('/createpost', cors({credentials:true , origin:'https://dos-bastardos-project.vercel.app/inicio/crearpost'}), upload.single('imagen'), async (req, res)=>{
+app.post('/createpost', upload.single('imagen'), async (req, res)=>{
 
     const { titulo, resumen, contenido } = req.body;
     const { token }= req.cookies;
