514fd2ebb41cd1ef8d184c0ac41a3969f90e7f66
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-06-11 04:20:20 -0500

    cors fix

--- backend-dosbastardos/api/index.js
+++ backend-dosbastardos/api/index.js
@@ -26,7 +26,7 @@
 
     //----------MIDDLEWARES---------------
     app.use(morgan('dev'));
-    app.use(cors({credentials:true , origin:'https://dos-bastardos-project.vercel.app/'}));
+    app.use(cors({credentials:true , origin:'https://dos-bastardos-project.vercel.app'}));
     app.use(express.json());
     app.use(cookieParser());
     app.use('/Imagenes', express.static(__dirname+'/Imagenes'))
