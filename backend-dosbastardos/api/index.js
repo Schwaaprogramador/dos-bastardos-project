5d1972238390f019503fd218ded1ac32743d7525
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-06-11 17:36:36 -0500

    fixes cors1.8

--- backend-dosbastardos/api/index.js
+++ backend-dosbastardos/api/index.js
@@ -35,16 +35,16 @@
 
                 }));
 
-    app.use((req, res, next) => {
-                    res.header("Access-Control-Allow-Origin", "https://dos-bastardos-project.vercel.app" , "https://dos-bastardos-project.vercel.app/createpost"); // update to match the domain you will make the request from
-                    res.header("Access-Control-Allow-Credentials", "true");
-                    res.header(
-                        "Access-Control-Allow-Headers",
-                        "Origin, X-Requested-With, Content-Type, Accept"
-                    );
-                    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-                    next();
-                });
+    // app.use((req, res, next) => {
+    //                 res.header("Access-Control-Allow-Origin", "https://dos-bastardos-project.vercel.app" , "https://dos-bastardos-project.vercel.app/createpost"); // update to match the domain you will make the request from
+    //                 res.header("Access-Control-Allow-Credentials", "true");
+    //                 res.header(
+    //                     "Access-Control-Allow-Headers",
+    //                     "Origin, X-Requested-With, Content-Type, Accept"
+    //                 );
+    //                 res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    //                 next();
+    //             });
    
 
     
