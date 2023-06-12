6a87f77b60649fca08768cb750f95a84d6fb1e30
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-06-11 21:02:06 -0500

    si

--- backend-dosbastardos/api/index.js
+++ backend-dosbastardos/api/index.js
@@ -34,7 +34,7 @@
 
     app.use((req, res, next) => {
                     
-                    res.setHeader('Access-Control-Allow-Origin', '*');
+                    res.setHeader('Access-Control-Allow-Origin', 'https://dos-bastardos-project.vercel.app');
             
                     next();
     });
