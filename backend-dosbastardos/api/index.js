2add36f17abeaf74ccc5e029d45f7fe943834984
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-06-11 21:08:26 -0500

    fixes cors13432423

--- backend-dosbastardos/api/index.js
+++ backend-dosbastardos/api/index.js
@@ -33,11 +33,11 @@
                 }));
 
     app.use((req, res, next) => {
-                    
                     res.setHeader('Access-Control-Allow-Origin', 'https://dos-bastardos-project.vercel.app');
-            
+                    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+                    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
                     next();
-    });
+    });;
 
     app.use(express.json());
     app.use(cookieParser());
