87b01c99fdd4cd60692fc5635f2eb2cf07859fc0
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-06-13 18:02:36 -0500

    cors6

--- dos-bastardos-project/src/componentes/CreatePost/CreatePost.jsx
+++ dos-bastardos-project/src/componentes/CreatePost/CreatePost.jsx
@@ -36,7 +36,7 @@ function CreatePost() {
           //---{withCredentials:true}---- para mandar la cookie
           console.log(payload)
 
-          const newPost = await axios.post('https://dos-bastardos-backend.up.railway.app/createpost', payload, {withCredentials:true} )
+          const newPost = await axios.post('https://dos-bastardos-backend.up.railway.app/createpost', payload )
           // const newPost = await axios.post('https://dos-bastardos-backend.up.railway.app/createpost', payload, {withCredentials:true})
 
           console.log(newPost.data)
