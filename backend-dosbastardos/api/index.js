6b103343dcba6e41695dc200c59d01b70f613b69
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-06-12 21:48:32 -0500

    last

--- backend-dosbastardos/api/index.js
+++ backend-dosbastardos/api/index.js
@@ -33,11 +33,27 @@
                 }));
 
     app.use((req, res, next) => {
-                    res.setHeader('Access-Control-Allow-Origin', 'https://dos-bastardos-project.vercel.app');
+                    res.setHeader('Access-Control-Allow-Origin', 'https://dos-bastardos-project.vercel.app, https://dos-bastardos-backend.up.railway.app/createpost');
                     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
                     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+                    res.setHeader("Access-Control-Allow-Credentials", "true");
                     next();
     });;
+
+
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", 'https://dos-bastardos-project.vercel.app, https://dos-bastardos-backend.up.railway.app/createpost'); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Credentials", "true");
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+        next();
+    });
+
+
+
 
     app.use(express.json());
     app.use(cookieParser());
