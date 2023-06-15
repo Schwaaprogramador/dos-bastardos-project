1939617ad4a86105453accbe8c3c10e6d9fcd5a0
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-06-14 19:13:46 -0500

    news

--- dos-bastardos-project/src/componentes/News/New.jsx
+++ dos-bastardos-project/src/componentes/News/New.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from './New.module.css';
-function New(name, description, image) {
+
+function New({name, description, imagen}) {
   return (
     <div>
 
-        <img className={styled.img} src={image} alt="imagen noticias" />
+        <img className={styled.img} src={imagen} alt="imagen noticias" />
         <div>
             <p>{name}</p>
         </div>
--- dos-bastardos-project/src/componentes/News/News.jsx
+++ dos-bastardos-project/src/componentes/News/News.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import New from './New';
 import axios from "axios";
+import styled from './News.module.css';
 
 function News() {
 
@@ -12,8 +13,8 @@ function News() {
         const pegarleAlApi = async () => {
             try {
 
-              const newsOne = await axios.get('https://newsapi.org/v2/top-headlines?country=co&apiKey=dd91e01931ae4f4ea0389ec12b4e1864');
-              setNoticias(newsOne.data);
+              const newsOne = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=dd91e01931ae4f4ea0389ec12b4e1864');
+              setNoticias(newsOne.data.articles);
              
             } catch (error) {
       
@@ -27,13 +28,20 @@ function News() {
            pegarleAlApi();   // es importante ejecutar la funcion.
     }, [])
 
+
+    console.log(noticias)
+
   return (
-    <div>
+    <div className={styled.container}>
         
-      {noticias.length>0 && noticias.map( noticia => <New   name={noticia.source.name} 
-                                                            description={noticia.title} 
-                                                            image={noticia.urlToImage}
-                                                        />)}
+        {  typeof noticias === 'string' 
+         ? 
+         <h2>cargando noticias</h2> 
+         : 
+         noticias.length > 0 
+         ? 
+         noticias.map( noticia => <New name={noticia.source.name} description={noticia.title} imagen={noticia.urlToImage}  />) 
+          : <h2> cargar las noticias </h2>}   
 
     </div>
   )
